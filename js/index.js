d0194478d39b6613c09dff13cead1166aac05466
Kartik <55845219+iamkartikp@users.noreply.github.com>
2020-02-24 19:15:55 +0530

    added countdown

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const clock = document.querySelector('.countdown');
+
+const count = new Date('March 20, 2020 00:00:00').getTime()
+const tick = () => {
+    const now = new Date().getTime();
+    const d = count - now;
+    
+    const days = Math.floor(d/(1000*60*60*24));
+    const h = Math.floor((d%(1000*60*60*24))/(1000*60*60));
+    const m =  Math.floor((d%(1000*60*60))/(1000*60));
+    const s = Math.floor((d%(1000*60))/(1000));
+
+    const html =`
+         <div> <span>${days}</span> <h6>Days</h6></div>
+         <div> <span>${h}</span> <h6>Days</h6></div>
+         <div> <span>${m}</span> <h6>Days</h6></div>
+         <div> <span>${s}</span> <h6>Days</h6></div>
+    `;
+
+    clock.innerHTML = html;
+};
+
+setInterval(tick, 1000);
